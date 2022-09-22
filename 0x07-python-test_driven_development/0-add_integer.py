f757a7bb73dff75bc8812b15f1f9f3d3333a0109
Saadat <samjosaadat@yahoo.com>
2022-09-22 15:59:31 +0000

    messge

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,11 @@
+def add_integer(a, b=98):
+    """raise issues if a and b arent ints or floats, returns a+b"""
+    if type(a) != int and type(a) != float:
+        raise TypeError("a must be an integer")
+    if type(b) != int and type(b) != float:
+        raise TypeError("b must be an integer")
+    if type(a) == float:
+        a = int(a)
+    if type(b) == float:
+        b = int(b)
+    return a + b
